b24d3bf691f17ea6b691c41fc804cdb2afada928
sashasemeniuk <sasha.sos.06032009@gmail.com>
2025-11-08 17:31:41 +0200

    homework bill gates

--- src/App.js
+++ src/App.js
@@ -2,24 +2,53 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const Quotes = [
+		'Patience is a key element of success',
+		'If you think your teacher is tough, wait till you get boss',
+		'Life is not fair — get used to it!',
+		'Success is a lousy teacher. Це seduces smart people in thinking they can not lose',
+		'Be nice to nerds..'
+	]
+	const Links = [
+		'https://en.wikipedia.org/wiki/Bill_Gates',
+		'https://www.gatesfoundation.org/',
+		'https://www.gatesnotes.com/',
+		'https://twitter.com/billgates'
+	]
+	return (
+		<div className="App">
+			<div className={'card bg-light m-3'}>
+				<h1 className={'card-title'}>Quotes of Bill Gates</h1>
+				<div className={'card-body'}>
+					<div>
+						<div className={'row'}>
+							<div className={'col-5'}>
+								<img className={'card-img'}
+									 src={'https://upload.wikimedia.org/wikipedia/commons/8/88/Bill_Gates_at_the_European_Commission_-_2025_-_P067383-987995_%28cropped%29.jpg'}/>
+							</div>
+							<div className={'col-7'}>
+								<p>William Henry Gates III (born October 28, 1955) is an American businessman and
+									philanthropist. A pioneer of the microcomputer revolution of the 1970s and 1980s, he
+									co-founded the software company Microsoft in 1975 with his childhood friend Paul
+									Allen.</p>
+								{Quotes.map(q =>
+									<div className={'card m-2 mt-0'}>
+										<h4 className={'h5'}>{q}</h4>
+										<span className={'card-subtitle'}>Bill Gates</span>
+									</div>
+								)}</div>
+						</div>
+					</div>
+					<div>
+						<h2>Useful Links</h2>
+						<div className={'row'}>
+							{Links.map(l => <a href={l}>{l}</a>)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
